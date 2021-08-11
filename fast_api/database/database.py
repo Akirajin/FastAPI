df2c264305436c8595f0fa2fed2a93dc22a2605a
Akirajin <tueda@uolinc.com>
2021-08-10 23:00:57 -0300

    crud

--- fast_api/database/database.py
+++ fast_api/database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost:5432/default_database"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
